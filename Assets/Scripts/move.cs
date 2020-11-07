8126ff0e93186cca6975c3f46b9a6d92b7a75bbe
Sbstyn <54476814+Sbstyn@users.noreply.github.com>
2020-11-07 12:57:20 +0100

    Smooth car movement

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -1,22 +1,34 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class move : NetworkBehaviour
 {
     public Rigidbody rb;
-    public float speed = 10;
+    public float speed = 0;
     public Camera cam;
+    public Text speedmeter;
+    public bool isOnGround;
 
-    public override void OnStartLocalPlayer()
+    public int model = 0;
+
+    //public Camera HUDCAM;
+
+    /*public override void OnStartLocalPlayer()
     {
-        GetComponent<MeshRenderer>().material.color = Color.blue;
+        //GetComponent<MeshRenderer>().material.color = Color.blue;
         Camera.main.GetComponent<CameraFollow>().target = transform; //Fix camera on "me"
 
+    }*/
+    private void Start()
+    {
+        GetComponentInChildren<Camera>().enabled = true;
     }
     void Awake()
     {
+        //HUDCAM.enabled = false;
         if (!isLocalPlayer)
         {
             cam.enabled = false;
@@ -24,34 +36,137 @@ public class move : NetworkBehaviour
     }
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (model == 1)
+            {
+                model = 0;
+            }
+            else
+            {
+                model = 1;
+            }
+        }
+        if (isOnGround == false)
+        {
+            if (speed >= 0f)
+            {
+                speed = speed - .02f;
+            }
+            else if (speed <= 0f)
+            {
+                speed = speed + .02f;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, transform.position.y + 10, transform.position.z);
+            rb.velocity = new Vector3(0, 0, 0);
+            gameObject.transform.rotation = new Quaternion(0, gameObject.transform.rotation.y, 0, 0);
+        }
+
+        if (gameObject.transform.position.y < -20)
+        {
+            speed = 0;
+            gameObject.transform.position = new Vector3(0, 12, 0);
+            rb.velocity = new Vector3(0, 0, 0);
+            gameObject.transform.rotation = new Quaternion(0, gameObject.transform.rotation.y, 0, 0);
+        }
+
         if (!isLocalPlayer)
         {
             return;
         }
 
-        float v = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.position += transform.forward * Time.deltaTime * speed;
+
+        if (isOnGround == true)
+        {
+
+            if (Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W))
+            {
+                if (speed == 0)
+                {
+                    speed = -.1f;
+                }
+                else
+                {
+                    if (speed > -10)
+                    {
+                        speed = speed - .05f;
+                    }
+                }
+            }
+            else if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+            {
+                if (speed == 0)
+                {
+                    speed = .1f;
+                }
+                else
+                {
+                    if (speed < 50)
+                    {
+                        speed = speed + .05f;
+                    }
+                }
+            }
+            else if (speed >= 0f)
+            {
+                speed = speed - .03f;
+            }
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                if (speed >= .5f)
+                {
+                    speed = speed - .2f;
+                }
+                else if (speed <= -.5f)
+                {
+                    speed = speed + .2f;
+                }
+                else
+                {
+                    speed = 0;
+                }
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                if (speed < 20)
+                {
+                    gameObject.transform.Rotate(0.0f, -speed / 10, 0.0f, Space.Self);
+                }
+                else
+                {
+                    gameObject.transform.Rotate(0.0f, -2, 0.0f, Space.Self);
+                }
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                if (speed < 20)
+                {
+                    gameObject.transform.Rotate(0.0f, speed / 10, 0.0f, Space.Self);
+                }
+                else
+                {
+                    gameObject.transform.Rotate(0.0f, 2, 0.0f, Space.Self);
+                }
+            }
+        }
+        
+        GetComponentInChildren<Camera>().enabled = true;
+
+        int finalspeed = (int) speed * 2;
+        speedmeter.text = finalspeed.ToString();
+
+        /*float v = Input.GetAxis("Vertical") * speed * Time.deltaTime;
 
         Vector3 FORWARD = transform.TransformDirection(Vector3.forward);
 
-        transform.localPosition += FORWARD * v;
-
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            speed = speed - .2f;
-        }
-        else
-        {
-            speed = 20;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            gameObject.transform.Rotate(0.0f, -speed / 20, 0.0f, Space.Self);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            gameObject.transform.Rotate(0.0f, speed / 20, 0.0f, Space.Self);
-        }
+        transform.localPosition += FORWARD * v;*/
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -59,6 +174,23 @@ public class move : NetworkBehaviour
         if (collision.collider.tag == "ground")
         {
             rb.velocity = new Vector3(0,0,0);
+            isOnGround = true;
         }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        isOnGround = false;
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.transform.tag == "ground")
+        {
+            isOnGround = true;
+            Debug.Log("pp");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        isOnGround = false;
     }
 }
