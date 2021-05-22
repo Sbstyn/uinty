f1ea4124cc03f35afb573611215fd9b3f760f1af
Sbstyn <sebestyen.marton3@gmail.com>
2021-05-22 14:26:29 +0200

    cleaning up and uploading mirroir

---
+++ GuitarHero/Assets/Scripts/cam.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class cam : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponentInChildren<Camera>().enabled = false;
+    }
+}
---
+++ GuitarHero/Assets/Scripts/move.cs
@@ -0,0 +1,214 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class move : NetworkBehaviour
+{
+    public Rigidbody rb;
+    public float speed = 0;
+    public Camera cam;
+    public Text speedmeter;
+    public bool isOnGround;
+    public int model = 0;
+
+    //public Camera HUDCAM;
+
+    /*public override void OnStartLocalPlayer()
+    {
+        //GetComponent<MeshRenderer>().material.color = Color.blue;
+        Camera.main.GetComponent<CameraFollow>().target = transform; //Fix camera on "me"
+
+    }*/
+    private void Start()
+    {
+        GetComponentInChildren<Camera>().enabled = true;
+        Physics.gravity = new Vector3(0, -20, 0);
+    }
+    void Awake()
+    {
+        //HUDCAM.enabled = false;
+        if (!isLocalPlayer)
+        {
+            cam.enabled = false;
+        }
+    }
+    void Update()
+    {
+        /*if (Input.GetKey(KeyCode.L) && Input.GetKeyDown(KeyCode.P))
+        {
+            if (GameObject.Find("arenaa").GetComponent<rna>().isrna == false)
+            {
+                GameObject.Find("arenaa").GetComponent<rna>().isrna = true;
+            }
+            else
+            {
+                GameObject.Find("arenaa").GetComponent<rna>().isrna = false;
+            }
+        }*/
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (model == 1)
+            {
+                model = 0;
+            }
+            else
+            {
+                model = 1;
+            }
+        }
+        if (isOnGround == false)
+        {
+            if (speed >= 0f)
+            {
+                speed = speed - .15f;
+            }
+            else if (speed <= 0f)
+            {
+                speed = speed + .15f;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, transform.position.y + 10, transform.position.z);
+            rb.velocity = new Vector3(0, 0, 0);
+            gameObject.transform.rotation = new Quaternion(0, gameObject.transform.rotation.eulerAngles.y, 0, 0);
+        }
+
+        if (gameObject.transform.position.y < -20)
+        {
+            speed = 0;
+            gameObject.transform.position = new Vector3(0, 12, 0);
+            rb.velocity = new Vector3(0, 0, 0);
+            gameObject.transform.rotation = new Quaternion(0, gameObject.transform.rotation.y, 0, 0);
+        }
+
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        transform.position += transform.forward * Time.deltaTime * speed;
+
+        if (isOnGround == true)
+        {
+
+            if (Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W))
+            {
+                if (speed == 0)
+                {
+                    speed = -.1f;
+                }
+                else
+                {
+                    if (speed > -10)
+                    {
+                        speed = speed - .05f;
+                    }
+                }
+            }
+            else if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+            {
+                if (speed == 0)
+                {
+                    speed = .1f;
+                }
+                else
+                {
+                    if (speed < 50)
+                    {
+                        speed = speed + .05f;
+                    }
+                }
+            }
+            else if (speed >= 0f)
+            {
+                speed = speed - .03f;
+            }
+            else if (speed <= 0f)
+            {
+                speed = speed + .03f;
+            }
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                if (speed >= .5f)
+                {
+                    speed = speed - .2f;
+                }
+                else if (speed <= -.5f)
+                {
+                    speed = speed + .2f;
+                }
+                else
+                {
+                    speed = 0;
+                }
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                if (speed < 20)
+                {
+                    gameObject.transform.Rotate(0.0f, -speed / 10, 0.0f, Space.Self);
+                }
+                else
+                {
+                    gameObject.transform.Rotate(0.0f, -2, 0.0f, Space.Self);
+                }
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                if (speed < 20)
+                {
+                    gameObject.transform.Rotate(0.0f, speed / 10, 0.0f, Space.Self);
+                }
+                else
+                {
+                    gameObject.transform.Rotate(0.0f, 2, 0.0f, Space.Self);
+                }
+            }
+        }
+        
+        GetComponentInChildren<Camera>().enabled = true;
+
+        int finalspeed = (int) speed * 2;
+        speedmeter.text = finalspeed.ToString();
+
+        /*float v = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        Vector3 FORWARD = transform.TransformDirection(Vector3.forward);
+
+        transform.localPosition += FORWARD * v;*/
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "ground")
+        {
+            rb.velocity = new Vector3(0, 0, 0);
+            isOnGround = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        isOnGround = false;
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.transform.tag == "ground")
+        {
+            isOnGround = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        isOnGround = false;
+        /*Vector3 f = new Vector3(rb.velocity.x, rb.velocity.y, rb.velocity.z);
+        speed = 0;
+        rb.AddForce(f.x, f.y, f.z);*/
+    }
+}
